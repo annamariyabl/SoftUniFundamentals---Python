1534a557fbf83ce8b07fe60ba9e0e66092d6d500
annamariyabl <annamariyabl@gmail.com>
2023-10-03 11:25:18 +0300

    hi

--- ThirdLab/ThirdExercise/ex3.py
+++ ThirdLab/ThirdExercise/ex3.py
@@ -1,8 +1,24 @@
 string = input()
 my_list = list(string.split(" "))
-teamA =
+teamA = []
+teamB = []
 
 for i in range (len(my_list)):
-    new_list = list(my_list[i].split('-'))
-    if new_list[0] == 'A':
+    if 'A' in my_list[i]:
+        teamA.append(my_list[i])
+        teamA = list(set(teamA))
+        if (11 - len(teamA)) < 7:
+            print(f"Team A - {11 - len(teamA)}; Team B - {11 - len(teamB)}")
+            print('Game was terminated')
+            break
+    elif 'B' in my_list[i]:
+        teamB.append(my_list[i])
+        teamB = list(set(teamB))
+        if (11 - len(teamB)) < 7:
+            print(f"Team A - {11 - len(teamA)}; Team B - {11 - len(teamB)}")
+            print('Game was terminated')
+            break
+else:
+    print(f"Team A - {11 - len(teamA)}; Team B - {11 - len(teamB)}")
+
 
