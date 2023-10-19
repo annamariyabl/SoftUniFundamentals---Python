f04a4b2a15e0af1a4f17b3b35f87054ca19cb05a
annamariyabl <annamariyabl@gmail.com>
2023-10-19 11:00:55 +0300

    add

--- 06. Programming Fundamentals Mid Exam Retake/treasure_hunt.py
+++ 06. Programming Fundamentals Mid Exam Retake/treasure_hunt.py
@@ -1,25 +1,36 @@
-list = input().split("!")
+loots = input().split('|')
 command = input()
-while command != 'Go Shopping!':
-    if 'Urgent' in command:
-        command = command.split()
-        if command[1] not in list:
-            list.insert(0,command[1])
-    elif 'Unnecessary' in command:
-        command1 = command.split()
-        if command1[1] in list:
-            list.remove(command1[1])
-    elif 'Correct' in command:
-        command1 = command.split()
-        if command1[1] in list:
-            index = list.index(command1[1])
-            list.pop(index)
-            list.insert(index,command1[2])
-    elif 'Rearrange' in command:
-        command1 = command.split()
-        if command1[1] in list:
-            list.remove(command1[1])
-            list.append(command1[1])
+while command != "Yohoho!":
+    command = command.split()
+    if "Loot" in command:
+        command.remove('Loot')
+        for i in command:
+            if i not in loots:
+                loots.insert(0,i)
+    elif "Drop" in command:
+        index = int(command[1])
+        if 0 <= index < len(loots):
+            add = loots[int(command[1])]
+            loots.pop(int(command[1]))
+            loots.append(add)
+    elif 'Steal' in command:
+        index = int(command[1])
+        if index<len(loots):
+            new_list = loots[(len(loots)-index):]
+            print(*new_list, sep=', ')
+            loots = loots[:(len(loots)-index)]
+        else:
+            new_list = loots
+            print(*new_list, sep=", ")
+            loots = []
     command = input()
 
-print(*list, sep=", ")
+if loots == []:
+    print('Failed treasure hunt.')
+else:
+    length = 0
+    for i in loots:
+        length += len(i)
+    length = length / len(loots)
+    print(f'Average treasure gain: {length:.2f} pirate credits.')
+
